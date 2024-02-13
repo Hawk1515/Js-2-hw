8f8c411bb6c66b0ad7829a3e872b9afe7c7ef00a
Farrukh <farrukhataev@gmail.com>
2024-02-13 09:23:02 +0300

    create html & js

---
+++ script.js
@@ -0,0 +1,74 @@
+var name = prompt('Ismingizni kiriting: ')
+console.log('mening ismim ' +name )
+
+var age = +prompt(' yoshingizni kiriting: ')
+console.log(' mening yoshim ' +age +' da ')
+
+var javob = 7
+var a = +prompt('5+2 =  ' )
+if(javob === a){
+ console.log("javob to`gri: " + a) 
+ alert("javob to`gri")
+}else {
+console.log("javob noto`gri: " + a +     "   to`gri javob:  " + javob )
+alert("javob noto`gri" )
+}
+var j2 = 12
+var b = +prompt('3+9 =  ' )
+if(j2 === b){
+ console.log("javob to`gri: " + b) 
+ alert("javob to`gri")
+}else {
+console.log("javob noto`gri: " +b  +    "    to`gri javob:  " +j2 )
+alert("javob noto`gri" )
+}
+var j3 = 5
+var c = +prompt('10-5 =  ' )
+if(j3 === c){
+ console.log("javob to`gri: " + c) 
+ alert("javob to`gri")
+}else {
+console.log("javob noto`gri: " + c  +    "    to`gri javob:  " +j3 )
+alert("javob noto`gri" )
+
+}
+var j4 = 12
+var d = +prompt('3*4 =  ' )
+if(j4 === d){
+ console.log("javob to`gri: " + d) 
+ alert("javob to`gri")
+}else {
+console.log("javob noto`gri: " + d +    "     to`gri javob:  " +j4 )
+alert("javob noto`gri" )
+
+}
+var j5 = 100
+var e = +prompt('10*10 =  ' )
+if(j5 === e){
+ console.log("javob to`gri: " + e) 
+ alert("javob to`gri")
+}else {
+console.log("javob noto`gri: " +e  +    "     to`gri javob:  " +j5 )
+alert("javob noto`gri" )
+
+}
+var j6 = .1
+var f = +prompt('13%4 =  ' )
+if(j6 === f){
+ console.log("javob to`gri: " + f)
+ alert("javob to`gri") 
+}else {
+console.log("javob noto`gri: " +f  + " to`gri javob:  " +j6 )
+alert("javob noto`gri" )
+}
+var j7 = .2
+var g = +prompt('10%4 =  ' )
+if(j7 === g){
+ console.log("javob to`gri: " + g) 
+ alert(" javob to`g`ri ")
+}else {
+console.log("javob noto`gri: " +g   + " to`gri javob:  " +j7 )
+alert(" szning javob to`g`ri ")
+
+}
+
